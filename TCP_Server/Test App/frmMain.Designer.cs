a3b25dc87315fc22e91196d991eeedf71fe0f7b5
flashasbi <flashasbi@gmail.com>
2015-08-29 18:45:38 +0300

    TCP_SERVER

---
+++ TCP_Server/Test App/frmMain.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace testerApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblConnected = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SeriPort = new System.IO.Ports.SerialPort(this.components);
+            this.tcpServer1 = new tcpServer.TcpServer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverIP = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtLog.Location = new System.Drawing.Point(6, 6);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(636, 237);
+            this.txtLog.TabIndex = 1;
+            // 
+            // txtText
+            // 
+            this.txtText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtText.Location = new System.Drawing.Point(6, 249);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtText.Size = new System.Drawing.Size(555, 91);
+            this.txtText.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(583, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tcp Port:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(567, 249);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 91);
+            this.btnSend.TabIndex = 21;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "TCP server:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(88, 393);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(70, 13);
+            this.lblStatus.TabIndex = 24;
+            this.lblStatus.Text = "Server Kapalı";
+            // 
+            // lblConnected
+            // 
+            this.lblConnected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblConnected.AutoSize = true;
+            this.lblConnected.BackColor = System.Drawing.SystemColors.Control;
+            this.lblConnected.Location = new System.Drawing.Point(278, 393);
+            this.lblConnected.Name = "lblConnected";
+            this.lblConnected.Size = new System.Drawing.Size(13, 13);
+            this.lblConnected.TabIndex = 32;
+            this.lblConnected.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(200, 393);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Bağlantı sayısı:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tcpServer1
+            // 
+            this.tcpServer1.Encoding = ((System.Text.Encoding)(resources.GetObject("tcpServer1.Encoding")));
+            this.tcpServer1.IdleTime = 50;
+            this.tcpServer1.IsOpen = false;
+            this.tcpServer1.MaxCallbackThreads = 100;
+            this.tcpServer1.MaxSendAttempts = 3;
+            this.tcpServer1.Port = -1;
+            this.tcpServer1.VerifyConnectionInterval = 0;
+            this.tcpServer1.OnConnect += new tcpServer.tcpServerConnectionChanged(this.tcpServer1_OnConnect);
+            this.tcpServer1.OnDataAvailable += new tcpServer.tcpServerConnectionChanged(this.tcpServer1_OnDataAvailable);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(656, 372);
+            this.tabControl1.TabIndex = 33;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.txtText);
+            this.tabPage1.Controls.Add(this.btnSend);
+            this.tabPage1.Controls.Add(this.txtLog);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(648, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Chat";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(648, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Robot";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(633, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "3001";
+            // 
+            // serverIP
+            // 
+            this.serverIP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.serverIP.AutoSize = true;
+            this.serverIP.BackColor = System.Drawing.SystemColors.Control;
+            this.serverIP.Location = new System.Drawing.Point(432, 393);
+            this.serverIP.Name = "serverIP";
+            this.serverIP.Size = new System.Drawing.Size(31, 13);
+            this.serverIP.TabIndex = 36;
+            this.serverIP.Text = "3001";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(380, 393);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Server IP:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "^\r\nİLERİ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SOLA\r\n<<\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(395, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 81);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SAĞA\r\n>>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 81);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "GERİ\r\nv";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(236, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 81);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "DUR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 422);
+            this.Controls.Add(this.serverIP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblConnected);
+            this.MinimumSize = new System.Drawing.Size(696, 461);
+            this.Name = "frmMain";
+            this.Text = "TCP Server [www.hasbisevinc.com]";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtText;
+        private tcpServer.TcpServer tcpServer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblConnected;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort SeriPort;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label serverIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ TCP_Server/Test App/frmMain.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testerApp
+{
+    public partial class frmMain : Form
+    {
+        public delegate void invokeDelegate();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openTcpPort()
+        {
+            tcpServer1.Close();
+            tcpServer1.Port = 3001;
+            tcpServer1.Open();
+
+            displayTcpServerStatus();
+        }
+
+        private void displayTcpServerStatus()
+        {
+            if (tcpServer1.IsOpen)
+            {
+                lblStatus.Text = "Server Açık";
+                lblStatus.BackColor = Color.Lime;
+                IPHostEntry host;
+                string localIP = "?";
+                host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    if (ip.AddressFamily.ToString() == "InterNetwork")
+                    {
+                        localIP = ip.ToString();
+                    }
+                }
+                serverIP.Text = localIP;
+            }
+            else
+            {
+                lblStatus.Text = "Server Kapalı";
+                lblStatus.BackColor = Color.Red;
+            }
+            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            
+            if(txtText.Text.Length > 0)
+                send();
+            txtText.Text = "";
+        }
+
+        private void send()
+        {
+            string data = "";
+
+            foreach (string line in txtText.Lines)
+            {
+                data = data + line.Replace("\r", "").Replace("\n", "") + "\r\n";
+            }
+            data = data.Substring(0, data.Length - 2);
+
+            tcpServer1.Send(data);
+
+            logData(true, data);
+        }
+
+       
+        private void sendData(char temp)
+        {
+            string data = "";
+            data = temp.ToString() + "\r\n";
+            data = data.Substring(0, data.Length - 2);
+            tcpServer1.Send(data);
+        }
+
+        public void logData(bool sent, string text)
+        {
+            txtLog.Text += "\r\n" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss tt") + (sent ? " YOLLANDI:\r\n" : " ALINDI:\r\n");
+            txtLog.Text += text;
+            txtLog.Text += "\r\n";
+            if (txtLog.Lines.Length > 500)
+            {
+                string[] temp = new string[500];
+                Array.Copy(txtLog.Lines, txtLog.Lines.Length - 500, temp, 0, 500);
+                txtLog.Lines = temp;
+            }
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            tcpServer1.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            openTcpPort();
+            timer1.Enabled = true;
+
+            
+        }
+
+        private void tcpServer1_OnDataAvailable(tcpServer.TcpServerConnection connection)
+        {
+            byte[] data = readStream(connection.Socket);
+
+            if (data != null)
+            {
+                string dataStr = Encoding.ASCII.GetString(data);
+
+                invokeDelegate del = () =>
+                {
+                    logData(false, dataStr);
+                };
+                Invoke(del);
+
+                data = null;
+            }
+        }
+
+        protected byte[] readStream(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            if (stream.DataAvailable)
+            {
+                byte[] data = new byte[client.Available];
+
+                int bytesRead = 0;
+                try
+                {
+                    bytesRead = stream.Read(data, 0, data.Length);
+                }
+                catch (IOException)
+                {
+                }
+
+                if (bytesRead < data.Length)
+                {
+                    byte[] lastData = data;
+                    data = new byte[bytesRead];
+                    Array.ConstrainedCopy(lastData, 0, data, 0, bytesRead);
+                }
+                return data;
+            }
+            return null;
+        }
+
+        private void tcpServer1_OnConnect(tcpServer.TcpServerConnection connection)
+        {
+            invokeDelegate setText = () => lblConnected.Text = tcpServer1.Connections.Count.ToString();
+
+            Invoke(setText);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            displayTcpServerStatus();
+            lblConnected.Text = tcpServer1.Connections.Count.ToString();
+        }
+
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendData('w');
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sendData('x');
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sendData('a');
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sendData('d');
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sendData('s');
+        }
+
+    }
+}
